76a32354bd252e4bc5e8c391ddb56d8e232c494c
xgh-jr <henggedeyingzi@qq.com>
2025-07-15 08:33:29 +0800

    【title】新增血条渐失效果 【commiter】xgh 【message】 1.血条组件里维护一个targetLosingPercent，和一个当前targetLosingPercent，当前Percent不为target时，每秒逐渐失去血量 2.不用Timer.DO是因为如果有两次losing效果，则Timer会同时执行两次效果，造成难以预知的后果

--- Assets/GameBase/UI/HealthBar.cs
+++ Assets/GameBase/UI/HealthBar.cs
@@ -15,21 +15,63 @@ namespace GameBase.UI
         private TextMeshProUGUI _textComponent;
         private GameObject _current;
         private RectTransform _currentRectTransform;
+        private GameObject _losing;
+        private RectTransform _losingRectTransform;
+
         private float _widthMax = 3f;
         private float _HPMax = 100f;
         private float _currHP;
+        private float _losingPercent = 1f;
+        private float _targetLosingPercent = 1f;
+
+        // Ã¿ÃëµôÑªËÙ¶È
+        public float losingSpeed = 0.5f;
         
 
         private void Awake()
         {
             _rectTransform = GetComponent<RectTransform>();
-            _widthMax = _rectTransform.rect.width;
+            
             _textObj = transform.Find("Text").gameObject;
             _textComponent = _textObj.GetComponent<TextMeshProUGUI>();
+            
             _current = transform.Find("Current").gameObject;
             _currentRectTransform = _current.GetComponent<RectTransform>();
+            
+            _losing = transform.Find("Losing").gameObject;
+            _losingRectTransform = _losing.GetComponent<RectTransform>();
 
-            //UIMgr.AttachToRoot(transform);
+            _widthMax = _rectTransform.rect.width;
+        }
+
+        private float Width
+        {
+            set => _currentRectTransform.offsetMax = new Vector2(value - _widthMax, _currentRectTransform.offsetMax.y);
+        }
+
+        private float LosingWidth
+        {
+            set
+            {
+                _losingRectTransform.offsetMax = new Vector2(value - _widthMax, _losingRectTransform.offsetMax.y);
+            }
+        }
+
+        private float LosingPercent
+        {
+            set
+            {
+                LosingWidth = Mathf.Clamp01(value) * _widthMax;
+                _losingPercent = value;
+            }
+            get => _losingPercent;
+        }
+
+        private void HealbarUpdOnce()
+        {
+            _textComponent.text = $"{_currHP} / {_HPMax}";
+            Percent = _currHP / _HPMax;
+            _targetLosingPercent = _currHP / _HPMax;
         }
 
         /// <summary>
@@ -40,11 +82,6 @@ namespace GameBase.UI
             set => Width = Mathf.Clamp01(value) * _widthMax;
         }
 
-        private void HealbarUpdOnce()
-        {
-            _textComponent.text = $"{_currHP} / {_HPMax}";
-            Percent = _currHP / _HPMax;
-        }
 
         public float CurrHP
         {
@@ -66,14 +103,15 @@ namespace GameBase.UI
             }
         }
 
-        private float Width
-        {
-            set => _currentRectTransform.offsetMax = new Vector2(value - _widthMax, _currentRectTransform.offsetMax.y);
-        }
+
 
         internal void _Update()
         {
             transform.position = _owner.HealthBarPosition;
+            if (_losingPercent > _targetLosingPercent)
+            {
+                LosingPercent = _losingPercent - losingSpeed * Time.deltaTime;
+            }
         }
 
         void IPoolableObject.OnInstantiate()
